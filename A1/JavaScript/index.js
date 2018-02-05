3e9450535dd9a84a85f9cd9da726da87468115d7
hailing-li <hailing@hawaii.edu>
2018-02-05 01:38:59 -1000

    Update index.js

--- A1/JavaScript/index.js
+++ A1/JavaScript/index.js
@@ -1,8 +1,1 @@
-$(".real").mouseenter(function(){
-        var image_name=$(this).data('image');
-        var imageTag='<div style="position:absolute;">'+'<img src="'+image_name+'" alt="image" height="100" />'+'</div>';
-        $(this).parent('div').append(imageTag);
-    });
-    $(".Your_class").mouseleave(function(){
-        $(this).parent('div').children('div').remove();
-    });
+
